0d8b4b4d1ea277d896fbec73393837757d55a627
Nahid Parvej <parvej>
2023-09-14 01:17:12 +0600

    added json data

--- src/main-section/Blogs.jsx
+++ src/main-section/Blogs.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
+import blog from "./blog";
+import Blog from "./blog";
 
 
 const Blogs = () => {
 
-    const [blogs,setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
 
     useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
+            .then(res => res.json())
+            .then(data => setBlogs(data))
 
-    },[])
+    }, [])
 
     return (
         <div>
             <h2>Blogs: {blogs.length}</h2>
+            {
+                blogs.map(blog => <Blog
+                    blog={blog}
+                ></Blog>)
+            }
         </div>
     );
 };
--- src/main-section/blog.jsx
+++ src/main-section/blog.jsx
@@ -1,11 +1,44 @@
 
+// import PropTypes from 'prop-types';
 
-const blog = () => {
+
+const Blog = ({ blog }) => {
+    const { title, cover, authorimg, author, date, time, icon, hashtag, mark } = blog;
     return (
-        <div>
-            
+        <div className="mb-5 bg-base-100 shadow-xl">
+            {/*  blogs cover photo */}
+            <img className="p-5" src={cover} alt="" />
+
+            {/* author  */}
+
+            <div className="p-5">
+                <div className="flex justify-between mx-3 mt-5">
+                    <div className="flex items-center gap-3">
+                        <img src={authorimg} alt="" />
+                        <div>
+                            <h4>{author}</h4>
+                            <h4>{date}</h4>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <p>{time}</p>
+                        <button> <img src={icon} alt="" /></button>
+                    </div>
+                </div>
+
+
+                {/* title */}
+
+                <h1 className="text-5xl mx-3 mt-5 mb-2  font-bold">{title}</h1>
+                <p className="mb-5 mx-3">{hashtag}</p>
+                <p className="mb-5 mx-3  text-blue-500 underline"><a href="">{mark}</a></p>
+            </div>
+
         </div>
     );
 };
 
-export default blog;
+// Blog.propTypes ={
+//     blog:PropTypes.object.isRequired
+// }
+export default Blog;
